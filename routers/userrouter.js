21fbee03d2f2fd774c722b7c601f0274accc70a4
workuseAsherdn <158446274+workuseAsherdn@users.noreply.github.com>
2024-04-27 16:06:04 +0530

    FinalBackend
    FinalBackend

---
+++ routers/userrouter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  verify,
+  Register,
+  Login,
+  User,
+  Logout,
+  LoggedIn,
+  updateProfile,
+  ForgotPassword,
+  passwordChange,
+  FindIdentity,
+  deleteUser,
+} = require("../controllers/authController");
+var router = express.Router();
+router.post("/verify", verify);
+router.post("/register", Register);
+router.post("/login", Login);
+router.get("/logout", Logout);
+router.put("/updateProfile", updateProfile);
+router.get("/user", User);
+router.post("/findIdentity", FindIdentity);
+router.get("/loggedIn", LoggedIn);
+router.post("/forgotPassword", ForgotPassword);
+router.post("/passwordChange", passwordChange);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
